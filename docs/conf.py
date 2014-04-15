7cb01fe49cb8aba36ae849d015da963bc1bbb4b1
Andrew Halberstadt <halbersa@gmail.com>
2014-04-15 17:48:08 -0400

    Use sphinx-bootstrap-theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import sys
 import os
 
-import mozilla_sphinx_theme
+import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -105,15 +105,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'mozilla'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'bootswatch_theme': 'cosmo',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [os.path.dirname(mozilla_sphinx_theme.__file__)]
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
